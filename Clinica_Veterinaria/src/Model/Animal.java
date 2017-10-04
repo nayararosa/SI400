2b1bb938783f8f1c51fd28ecf4fb45016026cc70
marzxz <maxi.biscaia54@gmail.com>
2017-10-04 15:03:41 -0300

    Remoção comentário

---
+++ Clinica_Veterinaria/src/Model/Animal.java
@@ -0,0 +1,34 @@
+
+package Model;
+
+public class Animal {
+    
+    private String nome_animal;
+    private int idade_animal;
+    private String sexo_animal;
+    private Cliente cliente;
+     
+    
+    public Animal(String[] dados){
+    
+    nome_animal = dados[0];
+    idade_animal = Integer.parseInt(dados[1]);
+    sexo_animal = dados[2];
+    }
+
+
+    public String[] Ret_Animal(){
+        String[] dados = new String[4];
+        
+        dados[0]= nome_animal;
+        dados[1] = String.valueOf(idade_animal);
+        dados[2] = sexo_animal;
+        return dados;
+       
+    }
+    
+    public String Con_Animal(){
+        return nome_animal;
+    }   
+}
+
