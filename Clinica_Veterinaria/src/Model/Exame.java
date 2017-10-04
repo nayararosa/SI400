5784b03548b806c67926a3adef432e9982bd0378
marzxz <maxi.biscaia54@gmail.com>
2017-10-04 15:56:54 -0300

    Classe Exame
    Classe exame armazena o tipo de exame socilicitado

---
+++ Clinica_Veterinaria/src/Model/Exame.java
@@ -0,0 +1,18 @@
+
+package Model;
+
+
+public class Exame {
+    
+    private String des_exame;
+    
+    
+    public void setExame(String ex){
+        des_exame = ex;
+    }
+    
+    public String getExame(){
+        return des_exame;
+    }
+    
+}
