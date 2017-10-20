8dc3bb1afb620c0deab79fc196348fc8bf545cb5
marzxz <maxi.biscaia54@gmail.com>
2017-10-20 21:18:32 -0200

    Implementação de métodos abstratos
    Classe que implementa métodos de interface polimórfica para a classe de controle do sistema "Controlador_Veterinaria.java"

---
+++ Clinica_Veterinaria/src/Control/interfacePolimorfica.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+/**
+ *
+ * @author Max
+ */
+public class interfacePolimorfica {
+    
+    
+    public interface Metodos
+            
+            
+    {
+      
+        
+       void writeAnimal(String cliente,String nome,String idade,String sexo, String especie);
+        
+       String[] consultaAnimal(String cliente,String nome);
+        
+        
+        
+    
+    
+    }
+    
+}
